72bf71cd7388ac8f4c00a3922d2b39755e08c69b
Duvva Vinay Kumar <vinaykumarduvva321@gmail.com>
2024-02-28 19:49:21 +0530

    Commit

---
+++ 5. odd or even/OddEvenChecker.java
@@ -0,0 +1,19 @@
+/*Check given number is Odd or Even using Modulus Operator */
+
+import java.util.Scanner;
+
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The Given Number is Even:" + number);
+        } else {
+            System.out.println("The Given number is Odd:" + number);
+        }
+        scanner.close();
+    }
+}
