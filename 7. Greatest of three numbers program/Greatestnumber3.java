722a7d35347bb5e77973d32294edb94ec37aa700
Duvva Vinay Kumar <vinaykumarduvva321@gmail.com>
2024-02-29 11:28:19 +0530

    Commit

---
+++ 7. Greatest of three numbers program/Greatestnumber3.java
@@ -0,0 +1,23 @@
+/* Using Ternary Operator */
+import java.util.Scanner;
+
+public class Greatestnumber3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter three numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        int greatest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        
+        System.out.println("The greatest number is: " + greatest);
+        
+        scanner.close();
+    }
+}
+ /* OUTPUT:
+Enter three numbers: 15 86 12
+The greatest number is: 86
+  */
