c349c25a1ef8eb1e43c04391b6d7a2c40e537d6d
Duvva Vinay Kumar <vinaykumarduvva321@gmail.com>
2024-02-29 11:51:44 +0530

    commit

---
+++ 8. Switch Condition Example/SwitchExample.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number between 1 to 7:");
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Firday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter the valid number");
+                break;
+        }
+        scanner.close();
+    }
+}
+
+/* Enter the number between 1 to 7: 4
+Thursday */
