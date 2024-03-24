1acf60797f99ebfb3df4375e9c0f21065154a8aa
Duvva Vinay Kumar <vinaykumarduvva321@gmail.com>
2024-03-24 09:46:40 +0530

    Commit

---
+++ 12. For loop example/ForExample.java
@@ -0,0 +1,27 @@
+/*
+ 	Demonstrate the for loop.
+ 	Call this file "ForTest.java".
+	*/
+    
+	public class ForExample {
+        public static void main(String[] args) {
+        int x;
+        
+       for(x = 0; x<=10; x = x+1)
+        System.out.println("This is x: " + x);
+        }
+   }
+
+/* Output:
+This is x: 0
+This is x: 1
+This is x: 2
+This is x: 3
+This is x: 4
+This is x: 5
+This is x: 6
+This is x: 7
+This is x: 8
+This is x: 9
+This is x: 10
+*/
